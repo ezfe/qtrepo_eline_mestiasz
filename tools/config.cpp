92c01f1a5d4571b8127ef200e05a0c7f83385456
Zura <prosperrriii@gmail.com>
2017-01-30 10:36:20 -0500

    working on Config class

---
+++ tools/config.cpp
@@ -0,0 +1,18 @@
+#include "config.h"
+
+#include <iostream>
+#include <map>
+#include <string>
+
+Config::Config(){
+
+}
+
+std::string Config::generateMap(){
+    std::map<std::string, int> brands;
+
+    std::string s = "Hello";
+    std::cout<<s;
+
+    return s;
+}
