ac4694e532896558c8eb5d58e301bfd3b9c068ab
Zura <prosperrriii@gmail.com>
2017-02-12 19:48:58 -0500

    Finished lab_03

--- unittest/main.cpp
+++ unittest/main.cpp
@@ -62,9 +62,37 @@ TEST_F(FooTest, TEST_FILE_NAMES) {
     EXPECT_EQ(c1.get_filepath(), c2.get_filepath());
 }
 
+TEST_F(FooTest, TEST_GENERATE_MAP) {
+    Config c1("./foo/bar/random.txt");
+    c1.generateMap();
+    EXPECT_EQ("applevalue", c1.getValue("apple"));
+    EXPECT_EQ("microsoftvalue", c1.getValue("microsoft"));
+    EXPECT_EQ("sonyvalue", c1.getValue("sony"));
+}
+
+TEST_F(FooTest, TEST_CLEAR_STATE) {
+    Config c1("./foo/bar/random.txt");
+    c1.generateMap();
+    c1.clearState();
+    EXPECT_EQ("", c1.getValue("apple"));
+    EXPECT_EQ("", c1.getValue("microsoft"));
+    EXPECT_EQ("", c1.getValue("sony"));
+}
+
 TEST_F(FooTest, TEST_LOG_INIT) {
     Log l1("./filename_special.txt", false);
     EXPECT_EQ("./filename_special.txt", l1.get_filepath());
+}
+
+TEST_F(FooTest, TEST_LOG_OPEN) {
+    Log l1("./filename_special.txt", true);
+    EXPECT_EQ(l1.isOpen(), true);
+}
+
+TEST_F(FooTest, TEST_LOG_CLOSE) {
+    Log l1("./filename_special.txt", true);
+    l1.close();
+    EXPECT_EQ(l1.isOpen(), false);
 }
 
 
