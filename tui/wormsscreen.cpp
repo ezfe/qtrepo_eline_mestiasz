ce896d44eb866d42bcae86f3c98a7db4a08ce217
Ezekiel Elin <ezekielelin@me.com>
2017-02-19 12:29:07 -0500

    Worms shows "Game Over"

--- tui/wormsscreen.cpp
+++ tui/wormsscreen.cpp
@@ -6,5 +6,10 @@ WormsScreen::WormsScreen() : Screen(){
 
 void WormsScreen::draw_screen(std::string str){
     clear();
-    mvprintw(0, 0, str.c_str());
+    if (this->engine->is_game_finished()) {
+        mvprintw(0, 0, "Game Over. Press q to exit...");
+        mvprintw(1, 0, str.c_str());
+    } else {
+        mvprintw(0, 0, str.c_str());
+    }
 }
