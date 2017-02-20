b2e5f3fd86b47825d59eb217c678d8f3dc04d309
Ezekiel Elin <ezekielelin@me.com>
2017-02-19 22:28:00 -0500

    Worms instructions

--- tui/wormsscreen.cpp
+++ tui/wormsscreen.cpp
@@ -6,10 +6,23 @@ WormsScreen::WormsScreen() : Screen(){
 
 void WormsScreen::draw_screen(std::string str){
     clear();
+    int starting_point = this->engine->get_width() + 10;
+    mvprintw(0, 0, str.c_str());
+
+    int row = 0;
     if (this->engine->is_game_finished()) {
-        mvprintw(0, 0, "Game Over. Press q to exit...");
-        mvprintw(1, 0, str.c_str());
+        row += 2;
+        mvprintw(0, starting_point, "Game Over:");
+        mvprintw(1, starting_point, "Press q to exit");
     } else {
-        mvprintw(0, 0, str.c_str());
+        mvprintw(0, starting_point, "Directions:" );
+        mvprintw(2, starting_point, "  k  ");
+        mvprintw(3, starting_point, "  |");
+        mvprintw(4, starting_point, "h- -l");
+        mvprintw(5, starting_point, "  |");
+        mvprintw(6, starting_point, "  j");
+        mvprintw(8, starting_point, "Hold shift to go further");
+        mvprintw(10, starting_point, "Commands:");
+        mvprintw(11, starting_point, "q: quit");
     }
 }
