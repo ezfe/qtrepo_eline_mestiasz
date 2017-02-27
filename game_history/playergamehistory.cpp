0f92abbb3bf5eaf6887b331a0f4f3140cd5130b9
Ezekiel Elin <ezekielelin@me.com>
2017-02-26 22:53:15 -0500

    PlayerGameHistory comments

--- game_history/playergamehistory.cpp
+++ game_history/playergamehistory.cpp
@@ -10,18 +10,34 @@ PlayerGameHistory::~PlayerGameHistory() {
     this->games.clear();
 }
 
+/*!
+ * \brief Query total number of games played across all players
+ * \return int total games played
+ */
 int PlayerGameHistory::number_games_played() {
     return games.size();
 }
 
+/*!
+ * \brief Query total number of players who have played games
+ * \return int total number of players
+ */
 int PlayerGameHistory::number_players_played() {
     return players.size();
 }
 
+/*!
+ * \brief Query average number of games per player
+ * \return double average games per player
+ */
 double PlayerGameHistory::avg_games_per_player() {
     return games.size() / players.size();
 }
 
+/*!
+ * \brief Query the top game score
+ * \return int the top game score
+ */
 int PlayerGameHistory::top_game_score() {
     int max = 0;
     for(auto game : games){
@@ -31,10 +47,19 @@ int PlayerGameHistory::top_game_score() {
     return max;
 }
 
+/*!
+ * \brief Query the average score for a specific player
+ * \param player to check
+ * \return double average score
+ */
 double PlayerGameHistory::avg_score_per_player(Player* player) {
     return player->get_total_score() / (player->get_game_history()->get_games().size() + 1);
 }
 
+/*!
+ * \brief Query the average game score across all games
+ * \return double average score
+ */
 double PlayerGameHistory::avg_game_score() {
    int sum = 0;
    for(auto game : games){
@@ -43,14 +68,27 @@ double PlayerGameHistory::avg_game_score() {
    return sum / games.size();
 }
 
+/*!
+ * \brief Get all the players
+ * \return std::vector<Player*> players
+ */
 std::vector<Player*> PlayerGameHistory::get_players(){
     return this->players;
 }
 
+/*!
+ * \brief Get all the games
+ * \return std::vector<Game*> games
+ */
 std::vector<Game*> PlayerGameHistory::get_games(){
     return this->games;
 }
 
+/*!
+ * \brief Add a game
+ * \param p the player who played
+ * \param g the game they played
+ */
 void PlayerGameHistory::add_game(Player* p, Game* g) {
     games.push_back(g);
     bool exists = false;
