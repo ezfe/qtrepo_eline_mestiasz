eee34e5df6e4b16808b1d80ad4590f15020239a1
Ezekiel Elin <ezekielelin@me.com>
2017-02-26 22:57:20 -0500

    Player comments

--- game_history/player.cpp
+++ game_history/player.cpp
@@ -23,32 +23,60 @@ Player::~Player() {
     delete recentGame;
 }
 
+/*!
+ * \brief Add a game to this player
+ * \param g game to add
+ */
 void Player::add_game(Game* g) {
     if(recentGame != nullptr) this->games->add_game(recentGame);
     this->recentGame = g;
 }
 
+/*!
+ * \brief Get the total score for this player (all games, summed).
+ * \return int total score
+ */
 int Player::get_total_score(){
     if(recentGame == nullptr) return 0;
     return games->get_total_score() + recentGame->get_score();
 }
 
+/*!
+ * \brief Get the player's first name
+ * \return std::string first name
+ */
 std::string Player::get_first_name(){
     return this->firstName;
 }
 
+/*!
+ * \brief Get the player's last name
+ * \return std::string last name
+ */
 std::string Player::get_last_name(){
     return this->lastName;
 }
 
+/*!
+ * \brief Get the player's address
+ * \return std::string address
+ */
 std::string Player::get_address(){
     return this->address;
 }
 
+/*!
+ * \brief Get the last game played
+ * \return Game* last game played
+ */
 Game* Player::get_recent_game(){
     return this->recentGame;
 }
 
+/*!
+ * \brief Get the other games
+ * \return GameHistory* other games
+ */
 GameHistory* Player::get_game_history(){
     return this->games;
 }
