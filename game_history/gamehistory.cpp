5179f26d3a2010e37a992e2c5f14f09464b69f3e
Ezekiel Elin <ezekielelin@me.com>
2017-02-26 22:58:14 -0500

    GameHistory comments

--- game_history/gamehistory.cpp
+++ game_history/gamehistory.cpp
@@ -8,14 +8,26 @@ GameHistory::~GameHistory(){
     this->games.clear();
 }
 
+/*!
+ * \brief Add a game
+ * \param game to add
+ */
 void GameHistory::add_game(Game* game) {
     this->games.push_back(game);
 }
 
+/*!
+ * \brief Get the games
+ * \return std::vector<Game*> games
+ */
 std::vector<Game*> GameHistory::get_games(){
     return this->games;
 }
 
+/*!
+ * \brief Get the total score of all the games
+ * \return int total score
+ */
 int GameHistory::get_total_score(){
     int totalScore = 0;
     for(auto game : games){
